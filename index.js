cde7542c1f787c163883a31ba8a7796bda0530c9
Lyoung0789 <Lyoung070789@gmail.com>
2020-05-23 23:27:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 31
+const num2= 2
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random()*10000)+1
+
+const mod = 29%5
+
+const max = Math.max(1,2,4,7,19,20,6,8,10)
